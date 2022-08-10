464cef9e87a6314fac06a66ca1452f7e324e7e57
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-11 00:48:24 +0500

    express server setup and logger from pino --express

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  port: 1337,
+  host: 'localhost',
+  dbUri: 'localhost:euriur',
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello world')
+import express from 'express'
+import config from 'config'
+import log from './logger'
+
+const port = config.get('port') as number
+const host = config.get('host') as string
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+  log.info(`Server is listing at http://${host}:${port}`)
+})
