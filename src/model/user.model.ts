745f85e94f85cab42aea5dc22cc42559965684f6
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-11 02:09:23 +0500

    compare and hash password

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 import config from 'config'
 
-export interface userDocuments extends mongoose.Document {
+export interface UserDocument extends mongoose.Document {
   name: string
   email: string
   password: string
@@ -29,6 +29,25 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
-const User = mongoose.model<userDocuments>('User', userSchema)
+userSchema.pre('save', async function (next) {
+  const user = this as UserDocument
+  if (!user.isModified('password')) return next()
+
+  const salt = await bcrypt.genSalt(10)
+
+  const hash = await bcrypt.hashSync(user.password, salt)
+
+  user.password = hash
+  return next()
+})
+
+userSchema.methods.comparedPassword = async function (
+  candidatePassword: string
+) {
+  const user = this as UserDocument
+  return bcrypt.compare(candidatePassword, user.password).catch((err) => false)
+}
+
+const User = mongoose.model<UserDocument>('User', userSchema)
 
 export default User
