e482d808bbb50389ba0d70747abe4deee9506898
adeelkbw <adeelnasirkbw@gmail.com>
2022-08-11 12:46:58 +0500

    user compare password service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,6 @@
 import User, { UserDocument } from '../model/user.model'
 import { DocumentDefinition } from 'mongoose'
+import { omit } from 'lodash'
 
 export async function createUser(input: DocumentDefinition<UserDocument>) {
   try {
@@ -7,4 +8,24 @@ export async function createUser(input: DocumentDefinition<UserDocument>) {
   } catch (error) {
     throw new Error('ERROR 400')
   }
+}
+
+export async function validateUserPassword({
+  email,
+  password,
+}: {
+  email: UserDocument['email']
+  password: string
+}) {
+  const user = await User.findOne({ email })
+  if (!user) {
+    return false
+  }
+
+  const isValid = await user.comparedPassword(password)
+
+  if (!isValid) {
+    return false
+  }
+  return omit(user.toJSON(), 'password')
 }
